dcbfe4e985a1330f4a456f96053830e13c0b2cb6
JulietteMadi <juliette.madinier@gmail.com>
2023-05-28 17:58:43 +0200

    Enable Tooltips

--- src/components/quizz/QuizzList.vue
+++ src/components/quizz/QuizzList.vue
@@ -19,7 +19,7 @@
 
     <!-- Quizz list -->
     <div class="row my-5 mx-auto text-center">
-        <div class=" col-12 col-sm-6 col-md-4 col-lg-3 py-3" v-for="quizz in filteredQuizz">
+        <div class=" col-12 col-sm-6 col-md-4 col-lg-3 py-3" v-for="(quizz, index) in filteredQuizz">
             <div class="card shadow">
                 <div class="card-header" style="height: 5rem;">
                     Some image
@@ -29,21 +29,29 @@
                 </div>
                 <div class="p-3 row">
                     <div class="col d-flex justify-content-center p-0">
-                        <button class="btn secundary-button mt-2">
+                        <button class="btn secundary-button mt-2" :id="'modify' + index" data-bs-toggle="tooltip"
+                            data-bs-custom-class="bg-tooltip" data-bs-original-title="Modifier"
+                            @mouseover="enableTooltip(`modify${index}`)">
                             <i class="bi bi-pencil-square"></i>
                         </button>
                     </div>
-                    <div class="col d-flex justify-content-center p-0">
+                    <div class="col d-flex justify-content-center p-0" :id="'cancel' + index" data-bs-toggle="tooltip"
+                        data-bs-custom-class="bg-tooltip" data-bs-original-title="Supprimer"
+                        @mouseover="enableTooltip(`cancel${index}`)">
                         <button class="btn secundary-button mt-2">
                             <i class="bi bi-trash3-fill"></i>
                         </button>
                     </div>
                     <div class="col d-flex justify-content-center p-0">
-                        <button class="btn secundary-button mt-2">
+                        <button class="btn secundary-button mt-2" :id="'share' + index" data-bs-toggle="tooltip"
+                            data-bs-custom-class="bg-tooltip" data-bs-original-title="Partager"
+                            @mouseover="enableTooltip(`share${index}`)">
                             <i class="bi bi-eye"></i>
                         </button>
                     </div>
-                    <div class="col d-flex justify-content-center p-0">
+                    <div class="col d-flex justify-content-center p-0" :id="'download' + index" data-bs-toggle="tooltip"
+                        data-bs-custom-class="bg-tooltip" data-bs-original-title="Télécharger le rapport"
+                        @mouseover="enableTooltip(`download${index}`)">
                         <button class="btn secundary-button mt-2">
                             <i class="bi bi-link-45deg"></i>
                         </button>
@@ -86,6 +94,14 @@ export default {
                     return name.includes(search);
                 });
             }
+        }
+    },
+    methods: {
+        enableTooltip(id) {
+            const element = document.getElementById(id)
+            console.log("enabling : " + id)
+            const tooltip = bootstrap.Tooltip.getOrCreateInstance(element);
+            tooltip.enable();
         }
     }
 }
